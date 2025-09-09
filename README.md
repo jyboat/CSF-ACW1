1b8bb05853ed7330faa3fc61a7aa4aee98d7cde1
josh <joshdebean@gmail.com>
2025-09-09 15:34:40 +0800

    proj setup

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # CSF-ACW1
+
+### Overview
+
+This project aims to apply steganography on Image and Audio files, 
+providing visualizations and comparisons for steganalysis.
+
+### Setup
+
+Run the following command:
+
+```
+pip install requirements.txt
+```
---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, url_for, redirect, request, flash
+from flask_toastr import Toastr
+
+
+app = Flask(__name__)
+
+
+toastr = Toastr(app)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        pass
+
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
